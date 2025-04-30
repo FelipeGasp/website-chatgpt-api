d3a971b32550a17476eb6ef41a747328c45847c0
FelipeGasp <felipe.gasparotto@sptech.school>
2025-04-30 18:56:45 -0300

    feat: added basic routing to test api

---
+++ main.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+
+const app = express();
+
+app.use(express.json())
+app.use(express.static("./"))
+
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+      res.render('index.html')
+})
+
+app.get('/prompt', (req, res)=>{
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
---
+++ prompt.js
@@ -0,0 +1,17 @@
+
+
+function sendPrompt(){
+    const titulo = document.getElementById("title").value;
+    const autor = document.getElementById("author").value;
+    console.log("Titulo: " + titulo);
+    console.log("Autor: " + autor);
+    console.log("Enviando prompt...");
+    fetch(`/prompt?titulo=${titulo}&autor=${autor}`,{
+        method: 'GET',
+        headers: {
+            contentType: 'application/json'
+        }
+    }).then((response)=>{
+        console.log("teste")
+    })
+}
